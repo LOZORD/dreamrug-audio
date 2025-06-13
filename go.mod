fc4146966a056e019bde6c1c7aaa4d67582d6f52
Leo Rudberg <ljr@google.com>
2025-06-13 19:57:36 -0400

    Added working audio!

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/lozord/dreamrug-audio
 
 go 1.24.2
 
-require github.com/golang/glog v1.2.5
+require (
+	github.com/go-audio/audio v1.0.0
+	github.com/go-audio/generator v0.0.0-20191129013639-fe5438877d8c
+	github.com/golang/glog v1.2.5
+	github.com/gordonklaus/portaudio v0.0.0-20250206071425-98a94950218b
+)
+
+require github.com/go-audio/transforms v0.0.0-20180121090939-51830ccc35a5
--- main.go
+++ main.go
@@ -7,18 +7,41 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
+	"github.com/go-audio/audio"
+	"github.com/go-audio/generator"
+	"github.com/go-audio/transforms"
 	log "github.com/golang/glog"
+	"github.com/gordonklaus/portaudio"
+)
+
+// Flags.
+var (
+	audioBufferSize = flag.Int("audio_buffer_size", 512, "The size of the audio buffer.")
 )
 
 func main() {
 	flag.Parse()
 	ctx := context.Background()
+
+	if err := portaudio.Initialize(); err != nil {
+		log.ExitContextf(ctx, "failed to initialize portaudio: %v", err)
+	}
+	defer func() {
+		log.InfoContextf(ctx, "terminating portaudio...")
+		if err := portaudio.Terminate(); err != nil {
+			log.ExitContextf(ctx, "failed to terminate portaudio: %v", err)
+		}
+	}()
+
 	log.Info("starting up an reading from stdin")
-	if err := doMain(ctx, os.Stdin); err != nil {
+	if err := doMain(ctx, os.Stdin, *audioBufferSize); err != nil {
 		log.Exitf("failed to run: %v", err)
 	}
 }
@@ -29,7 +52,7 @@ type sensorPayload struct {
 	UptimeMillis  int64 `json:"upt"`
 }
 
-const DATA_BUFFER_SIZE = 32
+const DATA_BUFFER_SIZE = 8 // originally 32
 
 type dataBuffer struct {
 	data [DATA_BUFFER_SIZE]int
@@ -69,30 +92,47 @@ func (db *dataBuffer) average(bias int) (float64, error) {
 	return avg, nil
 }
 
-func doMain(ctx context.Context, input io.Reader) error {
+func doMain(ctx context.Context, input io.Reader, audioBufferSize int) error {
 	scanner := bufio.NewScanner(input)
 
 	db := newDataBuffer()
-	for scanner.Scan() {
-		txt := scanner.Text()
 
-		log.V(5).InfoContextf(ctx, "got content: %s", txt)
-
-		var pld sensorPayload
-		if err := json.Unmarshal([]byte(txt), &pld); err != nil {
-			log.WarningContextf(ctx, "failed to unmarshall content to sensorPayload (skipping): %q: %v", txt, err)
-			continue
-		}
-
-		if err := handlePayload(ctx, pld, db); err != nil {
-			log.WarningContextf(ctx, "failed to handle payload: %v", err)
-			continue
-		}
+	buf := &audio.FloatBuffer{
+		Data:   make([]float64, audioBufferSize),
+		Format: audio.FormatMono44100,
 	}
+	currentNote := 440.0
+	osc := generator.NewOsc(generator.WaveSine, currentNote, buf.Format.SampleRate)
+	osc.Amplitude = 1
 
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("got error from scanner: %w", err)
-	}
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	fsrPercentage := make(chan int)
+
+	go func() {
+		for scanner.Scan() {
+			txt := scanner.Text()
+
+			log.V(5).InfoContextf(ctx, "got content: %s", txt)
+
+			var pld sensorPayload
+			if err := json.Unmarshal([]byte(txt), &pld); err != nil {
+				log.V(2).InfoContextf(ctx, "failed to unmarshall content to sensorPayload (skipping): %q: %v", txt, err)
+				continue
+			}
+
+			if err := handlePayload(ctx, pld, db, fsrPercentage); err != nil {
+				log.WarningContextf(ctx, "failed to handle payload: %v", err)
+				continue
+			}
+		}
+		// if err := scanner.Err(); err != nil {
+		// 	return fmt.Errorf("got error from scanner: %w", err)
+		// }
+	}()
+
+	runAudio(ctx, osc, buf, audioBufferSize, fsrPercentage, sig)
 
 	return nil
 }
@@ -100,7 +140,7 @@ func doMain(ctx context.Context, input io.Reader) error {
 const MAX_FSR_READING = 1024 - 1
 const WIDTH = 120
 
-func handlePayload(ctx context.Context, pld sensorPayload, db *dataBuffer) error {
+func handlePayload(ctx context.Context, pld sensorPayload, db *dataBuffer, fsrPercentage chan<- int) error {
 	uptime := time.Duration(pld.UptimeMillis) * time.Millisecond
 	log.V(2).InfoContextf(ctx, "got payload at uptime %s", uptime)
 	var mapped int = WIDTH * int(pld.FSRReading) / MAX_FSR_READING
@@ -113,5 +153,81 @@ func handlePayload(ctx context.Context, pld sensorPayload, db *dataBuffer) error
 		return fmt.Errorf("failed to get average for data buffer: %w", err)
 	}
 	log.V(2).InfoContextf(ctx, "got value: %d\trunning average:\t%f\n", pld.FSRReading, avg)
+
+	pct := int(math.Round(avg * 100.0 / MAX_FSR_READING))
+
+	fsrPercentage <- pct
+
 	return nil
+}
+
+func runAudio(ctx context.Context, osc *generator.Osc, buf *audio.FloatBuffer, audioBufferSize int, fsrPercentage <-chan int, sig chan os.Signal) {
+	gainControl := 0.0
+	currentVol := osc.Amplitude
+
+	out := make([]float32, audioBufferSize)
+	stream, err := portaudio.OpenDefaultStream(0, 1, 44100, len(out), &out)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stream.Close()
+
+	if err := stream.Start(); err != nil {
+		log.Fatal(err)
+	}
+	defer stream.Stop()
+
+	for {
+
+		select {
+		case pct := <-fsrPercentage:
+			gainControl = float64(pct) / 100.0
+			log.V(2).InfoContextf(ctx, "gain control: %f", gainControl)
+		case <-sig:
+			log.InfoContext(ctx, "stopping audio")
+			return
+		default:
+			log.V(5).InfoContextf(ctx, "nothing from the input channel!")
+		}
+
+		// populate the out buffer
+		if err := osc.Fill(buf); err != nil {
+			log.V(5).Infoln("error filling up the buffer")
+		}
+		// apply vol control if needed (applied as a transform instead of a control
+		// on the osc)
+		/*
+			if gainControl != 0 {
+				currentVol += gainControl
+				if currentVol < 0.1 {
+					currentVol = 0
+				}
+				if currentVol > 6 {
+					currentVol = 6
+				}
+				log.InfoContextf(ctx, "new vol %f.2", currentVol)
+				gainControl = 0
+			}
+		*/
+
+		currentVol = 6.0 * gainControl
+		log.V(1).InfoContextf(ctx, "current volume at %f", currentVol)
+
+		transforms.Gain(buf, currentVol)
+
+		f64ToF32Copy(out, buf.Data)
+
+		// write to the stream
+		if err := stream.Write(); err != nil {
+			log.ErrorContextf(ctx, "error writing to stream : %v\n", err)
+		}
+	}
+}
+
+// portaudio doesn't support float64 so we need to copy our data over to the
+// destination buffer.
+func f64ToF32Copy(dst []float32, src []float64) {
+	for i := range src {
+		dst[i] = float32(src[i])
+	}
 }
